9215f72215be9b1998012f2a2645bb39fa712aa1
Evandre Silva <evandresilva01@gmail.com>
2021-08-28 22:02:33 +0100

    Add project files.

---
+++ src/Kitadi.Web/Models/DashboardViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Angola_API_Package.Models;
+
+namespace Kitadi.Web.Models
+{
+    public class DashboardViewModel
+    {
+        public BNAInflationRate BNAInflationRate { get; set; }
+        public BNAMainExchangeRate BNAMainExchangeRate { get; set; }
+         
+    }
+}
